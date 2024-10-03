c41c87107565df9b20fc78e4c55aed28c169db46
ElenaStr13 <115697145>
2024-10-03 12:55:27 +0300

    Homework10

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,8 +3,8 @@ import { Router } from "express";
 import { userController } from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { commonMiddleware } from "../middlewares/common.middleware";
+import { fileMiddleware } from "../middlewares/file.middleware";
 import { UserValidator } from "../validators/user.validator";
-//import {fileMiddleware} from "../middlewares/file.middleware";
 
 const router = Router();
 
@@ -22,7 +22,7 @@ router.delete("/me", authMiddleware.checkAccessToken, userController.deleteMe);
 router.post(
   "/me/avatar",
   authMiddleware.checkAccessToken,
-  //fileMiddleware.isFileValid(),
+  fileMiddleware.isFileValid(),
   userController.uploadAvatar,
 );
 router.get(
