7062cbb794ee4d3348d4103f65f8e0b6308d5785
ElenaStr13 <115697145>
2024-10-11 11:20:13 +0300

    Homework 11

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -8,29 +8,32 @@ import { User } from "../models/user.model";
 
 class UserRepository {
   public async getList(query: IUserListQuery): Promise<[IUser[], number]> {
-    const filterObj: FilterQuery<IUser> = {};
-    const sortObj: { [key: string]: SortOrder } = {};
-    const { order, orderBy, search } = query;
-    if (search) {
+    const filterObj: FilterQuery<IUser> = {
+      // isVerified: true
+    };
+    if (query.search) {
       filterObj.name = { $regex: query.search, $options: "i" };
       // filterObj.$or = [
       //   { name: { $regex: query.search, $options: "i" } },
       //   { email: { $regex: query.search, $options: "i" } },
       // ];
     }
-    // TODO - Add sorting
-    if (orderBy) {
-      switch (orderBy) {
-        case UserListOrderByEnum.NAME:
-          sortObj.name = order;
-          break;
-        case UserListOrderByEnum.AGE:
-          sortObj.age = order;
-          break;
-        default:
-          throw new ApiError("Invalid orderBy", 400);
-      }
+
+    const sortObj: { [key: string]: SortOrder } = {};
+    switch (query.orderBy) {
+      case UserListOrderByEnum.NAME:
+        sortObj.name = query.order;
+        break;
+      case UserListOrderByEnum.AGE:
+        sortObj.age = query.order;
+        break;
+      case UserListOrderByEnum.CREATED:
+        sortObj.createdAt = query.order;
+        break;
+      default:
+        throw new ApiError("Invalid orderBy", 500);
     }
+
     const skip = query.limit * (query.page - 1);
     return await Promise.all([
       User.find(filterObj).sort(sortObj).limit(query.limit).skip(skip),
